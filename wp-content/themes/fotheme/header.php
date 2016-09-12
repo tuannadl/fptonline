5dcf6810e2f34bd3016385a63c8e7eef2852bcf9
tuannadl <tuannagithub@gmail.com>
2016-09-12 10:57:41 +0700

    fo db
    file sql

--- wp-content/themes/fotheme/header.php
+++ wp-content/themes/fotheme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset');?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> Nguyen Anh Tuan | 
+    <title> Nguyen Anh Tuan |
         <?php wp_title('|',true,'right');?>
         <?php bloginfo('name');?>
     </title>
