0dd08a8ff2107ab7900f3fa98fb8680745b4373a
tuan <tuannaphp@gmail.com>
2016-09-26 18:08:34 +0700

    Revert "update theme fotheme"
    This reverts commit d96efb5b9688670a65f7cfb275ece0d0a0549965.

--- wp-content/themes/fotheme/sidebar-menu-top.php
+++ wp-content/themes/fotheme/sidebar-menu-top.php
@@ -1,30 +1,30 @@
 <nav class="navbar-wrap">
     <div class="container">
         <ul class="nav-menu">
-            <li class="active"><a href="<?php echo bloginfo('url');?>" title="Trang chủ">Trang chủ</a>
+            <li class="active"><a href="index.html" title="Trang chủ">Trang chủ</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/san-pham-dich-vu';?>" title="S?n ph?m - D?ch v?">Sản phẩm - Dịch vụ</a>
+            <li><a href="#" title="Sản phẩm - Dịch vụ">Sản phẩm - Dịch vụ</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/tin-tuc';?>" title="Tin t?c">Tin tức</a>
+            <li><a href="#" title="Tin tức">Tin tức</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/gioi-thieu';?>" title="Gi?i thi?u">Giới thiệu</a>
+            <li><a href="#" title="Giới thiệu">Giới thiệu</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/quan-he-co-dong';?>" title="Quan h? c? ?ông">Quan hệ cổ đông</a>
+            <li><a href="#" title="Quan hệ cổ đông">Quan hệ cổ đông</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/tuyen-dung';?>" title="Tuy?n d?ng">Tuyển dụng</a>
+            <li><a href="#" title="Tuyển dụng">Tuyển dụng</a>
             </li>
-            <li><a href="<?php echo bloginfo('url').'/lien-he';?>" title="Liên h?">Liên hệ</a>
+            <li><a href="#" title="Liên hệ">Liên hệ</a>
             </li>
         </ul>
         <div class="wrap-social">
             <ul class="social-list">
-                <li >      <a href="<?php echo bloginfo('url');?>" title="Search" class="wi-icon wi-icon-search">search</a>
+                <li >      <a href="#" title="Search" class="wi-icon wi-icon-search">search</a>
                 </li>
                 <li class="item-social">      <a href="facebook.com" title="facebook" class="wi-icon wi-icon-fb">facebook</a>
                 </li>
                 <li>       <a href="google.com" title="goo+" class="wi-icon wi-icon-goo">goo+</a>
                 </li>
-                <li class="item-social"><a href="javascript:;" title="IE" class="wi-icon wi-icon-ie">IE</a>
+                <li class="item-social">       <a href="#" title="IE" class="wi-icon wi-icon-ie">IE</a>
                 </li>
             </ul>
         </div>
